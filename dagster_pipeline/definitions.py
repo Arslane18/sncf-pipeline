8ddc548273019c8df617b6d46fb53553a1708cd0
Arslane18 <ami54500@gmail.com>
2026-05-03 10:47:52 +0200

    refactor: loading all assets from dagster_pipeline instead of one

--- dagster_pipeline/definitions.py
+++ dagster_pipeline/definitions.py
@@ -1,12 +1,12 @@
 from dagster import Definitions
 from dagster import load_assets_from_package_module
 
-from dagster_pipeline.assets.ingestion import raw_sncf_disruptions
+from dagster_pipeline import assets
 from dagster_pipeline.config import SNCF_TOKEN
 from dagster_pipeline.resources import SNCFResource
 
 defs = Definitions(
-    assets=[raw_sncf_disruptions],
+    assets=load_assets_from_package_module(assets),
     resources={
         "sncf_api": SNCFResource(sncf_token=SNCF_TOKEN),
     },
