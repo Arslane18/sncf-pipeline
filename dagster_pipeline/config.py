483d9b52b6aeeec9ef19a8210af3718b58a9672a
Arslane18 <ami54500@gmail.com>
2026-05-05 10:21:11 +0200

    feat: added schedulers for ingestion job

--- dagster_pipeline/config.py
+++ dagster_pipeline/config.py
@@ -1,8 +1,12 @@
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
 load_dotenv()
 
-
+# SNCF API
 SNCF_API_URL = "https://api.sncf.com/v1/coverage/sncf/disruptions"
 SNCF_TOKEN = os.getenv("SNCF_TOKEN")
+
+# Database
+DB_PATH = "data/sncf.duckdb"
--- dagster_pipeline/schedules.py
+++ dagster_pipeline/schedules.py
@@ -1,9 +1,13 @@
-from dagster import ScheduleDefinition
-from .assets.ingestion import raw_sncf_disruptions
-
-
-daily_schedule = ScheduleDefinition(
-    name="daily_refresh",
-    cron_schedule="0 10 * * *",  # Runs at 10 AM daily (UTC and I am at UTC+2)
-    target=[raw_sncf_disruptions],
+from dagster import define_asset_job, ScheduleDefinition
+ 
+ingestion_job = define_asset_job(
+    name="ingestion_job",
+    selection=["fetch_sncf_disruptions", "save_raw_sncf_disruptions"],
+)
+ 
+daily_10h_schedule = ScheduleDefinition(
+    name="daily_10h_schedule",
+    job=ingestion_job,
+    cron_schedule="0 10 * * *",
+    description="Launch data ingestion every day at 10:00 AM.", # Timezone is UTC and I am UTC+2.
 )
