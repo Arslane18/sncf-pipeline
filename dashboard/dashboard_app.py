5536b27eef655d78efb8df02a61ba18e46598ca6
Arslane18 <ami54500@gmail.com>
2026-05-11 18:13:32 +0200

    disruptions by severity graph

---
+++ dashboard/dashboard_app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import duckdb
+import polars as pl
+
+st.title("🚆 SNCF Disruptions Dashboard")
+
+conn = duckdb.connect("data/sncf.duckdb")
+
+df = conn.execute("""
+SELECT *
+FROM mart_disruptions_by_serverity
+""").fetchdf()
+
+# KPI
+st.metric(
+    "Total disruptions",
+    int(df["nb_disruptions"].sum())
+)
+
+# Charts
+severity_df = (
+    df.groupby("severity")["nb_disruptions"]
+    .sum()
+    .reset_index()
+)
+
+st.subheader("Disruptions by severity")
+st.bar_chart(
+    severity_df.set_index("severity")
+)
